654653a883c6d628f2ce585653bf2028098d3163
waylau <waylau521@gmail.com>
2017-09-10 00:09:26 +0800

    init

--- docs/register-discover/register-discover.md
+++ docs/register-discover/register-discover.md
@@ -1,2 +1,9 @@
 # 微服务的注册与发现
 
+本章节，我们将创建一个`micro-weather-eureka-client` 作为客户端，并演示如何让将自身向注册服务器进行注册，让其可以其他服务都调用。
+
+## 开发环境
+
+* Gradle 4.0
+* Spring Boot 2.0.0.M3
+* Spring Cloud Netflix Eureka Client Finchley.M2
---
+++ samples/micro-weather-eureka-client/src/main/java/com/waylau/spring/cloud/Application.java
@@ -0,0 +1,25 @@
+package com.waylau.spring.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Configuration
+@ComponentScan
+//@EnableAutoConfiguration
+//@EnableEurekaClient
+@RestController
+public class Application {
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello world";
+    }
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
