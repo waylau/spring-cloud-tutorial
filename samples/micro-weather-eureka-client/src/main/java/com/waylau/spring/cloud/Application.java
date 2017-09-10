00feb72f83f4fccb03262d8d427fd1cf72fd2de1
waylau <waylau521@gmail.com>
2017-09-10 23:56:05 +0800

    update

--- samples/micro-weather-eureka-client/src/main/java/com/waylau/spring/cloud/Application.java
+++ samples/micro-weather-eureka-client/src/main/java/com/waylau/spring/cloud/Application.java
@@ -2,6 +2,7 @@ package com.waylau.spring.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Configuration
 @ComponentScan
-//@EnableAutoConfiguration
-//@EnableEurekaClient
+@EnableAutoConfiguration
+@EnableEurekaClient
 @RestController
 public class Application {
 
